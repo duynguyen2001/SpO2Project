b0bab4e049759c48368410997b4d12edc3a18ce1
khuynh22 <57774658+khuynh22@users.noreply.github.com>
2021-11-30 23:02:45 -0600

    Update receiver.ino

--- Arduino files/receiver.ino
+++ Arduino files/receiver.ino
@@ -1,7 +1,14 @@
+/* This is the peripheral part of the Project
+ * This code will take the data received by the Wi-Fi module and process it 
+ * The data will help decide to turn on the emergency signal or not (buzzer and LED) 
+ */
+
+// library
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 
+// variables declaration for the peripheral
 RF24 radio (7,8);
 const uint64_t address =  0xF0F0F0F0E1LL;
 const int Button = 5;
@@ -11,9 +18,11 @@ int ButtonState = 0;
 int BuzzerState = 1;
 int lastButtonState = 0;
 int lastBuzzerState = 0;
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); 
+
+// set up function
+void setup() {:
+  Serial.begin(9600);
+  // setting up the wifi module
   radio.begin();
   radio.setAutoAck(1);
   radio.setRetries(15,15);
@@ -22,27 +31,34 @@ void setup() {
   radio.setChannel(10);
   radio.openReadingPipe(1,address);
   radio.startListening();
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(5,INPUT);
+  // set pin mode for the buzzer, LED and the button
+  pinMode(2,OUTPUT); // LED
+  pinMode(3,OUTPUT); // buzzer
+  pinMode(5,INPUT); // button
 }
 
-void loop() { 
+void loop() {
+  // readin the data for button
   ButtonState = digitalRead(Button);
-  // put your main code here, to run repeatedly:
   if (radio.available()) {
+    // read the array of data from the transmitter
     radio.read(&msg,sizeof(msg));
+    // display the value of the array from the transmitter onto the serial display 
     Serial.print("1234:\t");
     Serial.print(msg[0]);
     Serial.print("\t");
     Serial.print(msg[1]);
     Serial.print("\t");
     Serial.print(msg[2]);
-    Serial.print("\t");
-    Serial.println(msg[3]);
+
+    // if the heart rate per minute are below 60 or over 120
+    // or if the level of SpO2 goes below 70%
+    // we turn on the buzzer and the LED for alarm
     if ( msg[0] < 60 || msg[0] > 120 || msg[1] < 70) {
+      // writein the value for LED and buzzer
       digitalWrite(2, LEDState);
       digitalWrite(3, BuzzerState);
+      // check last button state to turn off the buzzer and the LED properly
       if (ButtonState != lastButtonState) {
         lastButtonState = ButtonState;
         if (ButtonState == LOW) {
@@ -50,7 +66,7 @@ void loop() {
           digitalWrite(2, LEDState);
         }
         if (ButtonState == LOW) {
-          BuzzerState= (BuzzerState== HIGH) ? LOW : HIGH;
+          BuzzerState = (BuzzerState == HIGH) ? LOW : HIGH;
           digitalWrite(3, BuzzerState);
         }
       }
